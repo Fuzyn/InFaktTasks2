64139d25dd793fc1945cf36b7c54e2bb9152845c
michaelWierzbicki <michael.wierzbicki@gdsoft.info>
2023-09-26 20:54:45 +0200

    1. count array element 2. create password validation

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,10 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    combined_regex = Regexp.new("^(?=.{6,24}$)(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%&*+=:;?<>])(?!.*(.)\1\1)")
+    !combined_regex.match(@password).nil?
+  end
 end
